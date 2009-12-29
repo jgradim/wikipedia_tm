9db8d0bd96e9ddc45c044cd414ec8cb3a4801b29
João Valente <jvalente@lglw65.(none)>
2009-12-29 15:34:55 +0000

    Extract events and respective dates in a defined interval of years from wikipedia.

--- wikipedia.rb
+++ wikipedia.rb
@@ -1,7 +1,22 @@
+require 'date'
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/1974"))
 
-puts doc.xpath("//span[starts-with(@id,'Births')]//../../preceding::span[@class='mw-headline']/parent::h3/following-sibling::ul[1]").size
+for i in 1912..1912
+	doc = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/#{i}"))
+
+	doc.xpath("//span[starts-with(@id,'Births')]//../../preceding::span[@class='mw-headline']/parent::h3/following-sibling::ul[1]/li").each do |event|
+
+		date = Date.parse('1912 ' + f.at_css('.mw-formatted-date').text) rescue "rescued"
+
+		if event.at_css('ul') == nil
+			puts event.text.gsub(/^.* – /, '')
+		else
+			event.css("li").each do |sub_event|
+				puts sub_event.text
+			end
+		end
+	end
+end
