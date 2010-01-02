7ebf76f153a7b979b6543809e2b5a9435d25b999
João Gradim <joao.gradim@gmail.com>
2010-01-02 21:27:07 +0000

    v 0.9 of of local extraction

--- sp.rb
+++ sp.rb
@@ -9,20 +9,19 @@ def find_location(tree)
 	
 	while it.hasNext
 		node = it.next
-		puts node.to_s unless node.to_s.match(/\(.*\)/)
 		if node.label.to_s === 'PP'
 			location = node.toString #not optimal, but contains location
 			break
 		end
 	end
 	
-	puts location
-	m = location.match(/([\w,]+)(?=\))/) # works in rubular, no idea why it doesn't work here
-	puts m.size
+	location.scan(/([\w,]+)(?=\))/).join(" ").gsub(/^in /i, '').gsub(' ,', ',')
 end
 
 parser = StanfordParser::LexicalizedParser.new
 
-tree = parser.apply("The 1988 Summer Olympics are held in Seoul, South Korea.")
+sentence = ARGV[0] || "The 1988 Summer Olympics are held in Seoul, South Korea."
 
-find_location(tree)
+tree = parser.apply(sentence)
+
+puts find_location(tree)
