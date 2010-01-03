f3c9d054d54e980b4b5da9e36831412142cc2273
João Gradim <joao.gradim@gmail.com>
2010-01-03 16:06:37 +0000

    Added another phrasal structure for local extraction

--- sp.rb
+++ sp.rb
@@ -13,31 +13,31 @@ def find_location(tree)
 		node = it.next
 		if node.label.to_s == 'PP' and is_location(node.iterator)
 			location = node.toString #not optimal, but contains location
-			puts "encontrei"
-			puts is_location(node.iterator)
 			break
 		end
 	end
-	location.scan(/([\w,]+)(?=\))/).join(" ").gsub(/^in |^from |^of |^to /i, '').gsub(' ,', ',')
+	puts location.scan(/([\w,]+)(?=\))/).join(" ")
+	location.scan(/([\w,]+)(?=\))/).join(" ").gsub(/^in |^from |^of |^to |^at |^the /i, '').gsub(' ,', ',')
 end
 
 # determines if a subtree is a location
 def is_location(it)
-	i = 0
 	nodes = []
-	while i < 5 and it.hasNext
+	while nodes.size < 5 and it.hasNext
 		label = it.next.label.to_s
 		if label.match(/[A-Z]{2,3}/)
-			i += 1
 			nodes << label
-			#break	if i == 3 and label == "NNP" # Stop matching, successful
+			break	if nodes.size == 4 and label == "NNP" # Stop matching, successful
 		end
 	end
+	
+	puts "nodes: "+nodes.inspect
 	
 	[["PP", "IN", "NP", "NNP"],
 	 ["PP", "TO", "NP", "NNP"],
 	 ["PP", "IN", "NP", "NP", "NNP"],
-	 ["PP", "TO", "NP", "NP", "NNP"]].include? nodes
+	 ["PP", "TO", "NP", "NP", "NNP"],
+	 ["PP", "IN", "NP", "DT", "NNP"]].include? nodes
 end
 
 def coords_from_location(location)
@@ -61,4 +61,4 @@ tree = parser.apply(sentence)
 loc = find_location(tree)
 
 puts "Location    : "+loc
-puts "Coordinates : "+coords_from_location(loc).join(", ")
+puts "Coordinates : "+coords_from_location(loc).join(", ") if loc
