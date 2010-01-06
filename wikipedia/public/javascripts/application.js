9daeaedef400e404ed09baf22f9179289115d01a
João Gradim <joao.gradim@gmail.com>
2010-01-06 18:53:19 +0000

    Initial version of the visualization

--- wikipedia/public/javascripts/application.js
+++ wikipedia/public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+	
+	var map;
+	if (GBrowserIsCompatible()) {
+		map = new GMap2(document.getElementById("map"));
+		map.setCenter(new google.maps.LatLng(0.0, 0.0), 2);
+		map.addControl(new GLargeMapControl3D());
+	}
+	
+});
