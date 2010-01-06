716ebc062b7c85590f743f7db03b9f251afee4bb
João Gradim <joao.gradim@gmail.com>
2010-01-06 18:53:28 +0000

    routes

--- wikipedia/config/routes.rb
+++ wikipedia/config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
+	map.resources :events, :collection => { :search => :get } 
+
 	map.root :controller => 'home', :action => 'index'
 
   map.connect ':controller/:action/:id'
