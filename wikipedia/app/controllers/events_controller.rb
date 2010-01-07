21b9a298aa92360f41dc5aa62b4b5675daa65997
João Gradim <joao.gradim@gmail.com>
2010-01-07 11:29:55 +0000

    Added searching for people involved in events.

--- wikipedia/app/controllers/events_controller.rb
+++ wikipedia/app/controllers/events_controller.rb
@@ -3,7 +3,11 @@ class EventsController < ApplicationController
 	## GET
 	def search
 		categories = params[:categories] || []
-		@events = Event.find(:all, :conditions => [ "category IN (?) AND description LIKE ? AND YEAR(date) = ?", categories, "%#{params[:keywords]}%", params[:year]])
+		@events = Event.find(:all, :conditions => [ "category IN (?) AND people like ? AND description LIKE ? AND YEAR(date) = ?",
+			categories,
+			"%#{params[:people]}%",
+			"%#{params[:keywords]}%",
+			params[:year]])
 		@newest_year = Event.newest_year
 		@oldest_year = Event.oldest_year
 		@year = params[:year]
